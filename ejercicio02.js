b0297f19b08c5fb1029df230680d4452b0d5578f
Francisco Cervantes <francisco2412130@hybridge.education>
2026-02-04 19:57:30 -0600

    ejercicios completados

--- ejercicio02.js
+++ ejercicio02.js
@@ -2,9 +2,13 @@
 // Declara una función llamada "obtenerPI" que retorne el valor 3.1416
 
 //define tu función aquí
+function obtenerPI(){
+    const pi = 3.1416;
+    return pi;
+}
 
 
-// No modificar
+// No modificars
 // Exporta la función para que pueda ser utilizada en otros archivos
 // Como ves, tu función debe tener el nombre obtenerPI
 module.exports = {
--- ejercicio05.js
+++ ejercicio05.js
@@ -2,7 +2,11 @@
 // Declara una función llamada "calcularAreaRectangulo" que acepte dos parámetros, "ancho" y "alto", y retorne el área del rectángulo
 
 //escribe tu función aquí. Recuerda que el nombre exacto de la función es "calcularAreaRectangulo"
+function calcularAreaRectangulo (ancho, alto){
+    return (ancho*alto)/2
+}
 
+console.log(calcularAreaRectangulo(19,18))
 
 // Exporta la función para que pueda ser utilizada en las pruebas
 module.exports = {
